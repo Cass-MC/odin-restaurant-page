49316d9cef60a2f3842f040f2a9eb5e3eb60282d
Cassandra Cawley <cassandrabook09@gmail.com>
2023-06-15 13:48:56 -0700

    basic header recreation done

--- src/home/homeLoad.js
+++ src/home/homeLoad.js
@@ -1,9 +1,29 @@
 //Create header, then create hero, then create info, then create footer, Then append them and export that function
 
-//Create header
 function createHeader () {
     const head = document.createElement('div');
     head.id = 'header';
+
+    const logo = document.createElement('div');
+    logo.classList.add('headerNav');
+    logo.textContent = 'logo';
+
+    const menu = document.createElement('div');
+    menu.classList.add('headerNav');
+    menu.textContent = 'menu';
+
+    const order = document.createElement('div');
+    order.classList.add('headerNav');
+    order.textContent = 'order';
+
+    const about = document.createElement('div');
+    about.classList.add('headerNav');
+    about.textContent = 'about';
+
+    head.appendChild(logo);
+    head.appendChild(menu);
+    head.appendChild(order);
+    head.appendChild(about);
     return head;
 }
 
