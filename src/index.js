8e14f804a9844c859cb866e589e662c67e956ec8
Cassandra Cawley <cassandrabook09@gmail.com>
2023-06-20 12:39:56 -0700

    hooked up menuLoad to index

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 import './style.css';
 import {appendElements as homeLoad} from './home/homeLoad.js';
+import {appendElements as menuLoad} from './menu/menuLoad.js';
 
 homeLoad();
+
+function addListeners() {
+    const menu = document.getElementsByClassName('headerNav')[1];
+    menu.addEventListener('click', () => {
+        menuLoad();
+    });
+    
+}
+
+addListeners();
