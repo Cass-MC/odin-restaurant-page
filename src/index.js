c64e61308624b1088de79fb5adc6efa6e6a95da8
Cassandra Cawley <cassandrabook09@gmail.com>
2023-06-29 10:15:14 -0700

    added homeLoad on clicking logo

--- src/index.js
+++ src/index.js
@@ -4,22 +4,40 @@ import {appendElements as menuLoad} from './menu/menuLoad.js';
 
 homeLoad();
 
-function wipePage() {
+function wipePage(page = 'notMain') {
     const contentNode = document.getElementById('content');
 
-    for (let i = 0; i < contentNode.children.length; i++) {
-        const currentNode = contentNode.children[i];
-        if (currentNode.id == 'header' || currentNode.id == 'footer') { continue; }
-        else { contentNode.removeChild(currentNode); i--; }
+    if (page == 'notMain') {
+        for (let i = 0; i < contentNode.children.length; i++) {
+            const currentNode = contentNode.children[i];
+            if (currentNode.id == 'header' || currentNode.id == 'footer') { continue; }
+            else { contentNode.removeChild(currentNode); i--; }
+        }
+    }
+
+    if (page == 'main') {
+        for (let i = 0; i < contentNode.children.length; i++) {
+            const currentNode = contentNode.children[i];
+            contentNode.removeChild(currentNode);
+            i--;
+        }
     }
 
 }
 
 function addListeners() {
+    const main = document.getElementsByClassName('headerNav')[0];
+    main.addEventListener('click', () => {
+        wipePage('main');
+        homeLoad();
+        addListeners();
+    })
+
     const menu = document.getElementsByClassName('headerNav')[1];
     menu.addEventListener('click', () => {
         wipePage();
         menuLoad();
+        addListeners();
     });
     
 }
